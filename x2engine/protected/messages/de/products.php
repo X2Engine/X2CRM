672d883f2bc244b25152aab619484e062cdbbffb
Gabor Tjong A Hung <G.V.TjongAHung+github@gmail.com>
2015-03-06 10:32:31 -0300

    Update products.php
    Translated to dutch instead of german

--- x2engine/protected/messages/de/products.php
+++ x2engine/protected/messages/de/products.php
@@ -1,27 +1,27 @@
 <?php
 return array (
 // Product Attributes
-'Inventory'=>'Inventar',
-'Product Information'=>'Produktinformationen',
+'Inventory'=>'Inventaris',
+'Product Information'=>'Produktinformatie',
 'USD'=>'USD',
 
 // Product Actions
-'Product List'=>'Produkt-Liste',
+'Product List'=>'Produktenlijst',
 
 // Product Titles
 'Product'=>'Produkt',
 'Product:'=>'Produkt:',
-'Update Product: {name}'=>'Aktualisieren Produkt: {name}',
-'Create New Product'=>'Neues Produkt anlegen',
-'Update Product'=>'Aktualisieren Produkt',
+'Update Product: {name}'=>'Bijwerken Produkt: {name}',
+'Create New Product'=>'Nieuw Produkt aanmaken',
+'Update Product'=>'Bijwerken Produkt',
 
-'Adjustments'=>'Anpassungen',
+'Adjustments'=>'Aanpassingen',
 // Product Table
-'Line Item'=>'Einzelposten',
-'Unit Price'=>'Einzelpreis',
+'Line Item'=>'Regelitem',
+'Unit Price'=>'Eenheidsprijs',
 
 // Misc Text
-'Import Products'=>'Produkte importieren',
-'Export Products'=>'Export-Produkte',
+'Import Products'=>'Produkten importeren',
+'Export Products'=>'Produkten exporteren',
 'NameId'=>'NameID',
 );
