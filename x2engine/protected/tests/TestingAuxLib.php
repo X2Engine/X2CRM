df2e93ee3d7b0f75292823cd11682102299c5792
Jake Houser <jake@x2engine.com>
2016-07-06 15:48:39 -0700

    Temporarily remove checks from restoreController function

--- x2engine/protected/tests/TestingAuxLib.php
+++ x2engine/protected/tests/TestingAuxLib.php
@@ -326,12 +326,12 @@ class TestingAuxLib  {
     }
 
     public static function restoreController () {
-        if(isset(self::$_oldController)){
+        //if(isset(self::$_oldController)){
             Yii::app()->controller = self::$_oldController;
-        }
-        if(isset(self::$_oldServerVals)){
+        //}
+        //if(isset(self::$_oldServerVals)){
             $_SERVER = self::$_oldServerVals;
-        }
+        //}
     }
 
     public static function setConstant ($constName, $val) {
