cf0587ec12c16680ef4bcf08dd9004d946d857a0
Jake Houser <jake@x2engine.com>
2016-08-02 21:34:42 +0000

    makeBuild.py: Version updated, and tag added: 6.0.3

--- x2engine/protected/config/X2Config.php
+++ x2engine/protected/config/X2Config.php
@@ -5,8 +5,8 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $dbname = 'x2engine';
-$version = '6.0.2';
-$buildDate = '1468445092';
+$version = '6.0.3';
+$buildDate = '1470173682';
 $updaterVersion = '6.0';
 $language='en';
 ?>
