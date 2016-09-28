69db0748fde4ce8606c0a57a3ea7d67349165425
Jake Houser <jake@x2engine.com>
2016-09-27 17:01:53 -0700

    Remove debug statement

--- x2engine/protected/modules/contacts/controllers/ContactsController.php
+++ x2engine/protected/modules/contacts/controllers/ContactsController.php
@@ -521,7 +521,6 @@ class ContactsController extends x2base {
                 $locationCodes[] = $tempArr;
             }
         }
-        printR($locationCodes,true);
         /*
          * $locationCodes[0] is the first location on the map and where the map
          * will be centered. If we have a Contact ID, center it on that contact's
