631d650ab5c1e3ac5ad5ce87943bc4fa633a9ba1
Raymond Colebaugh <raymond@x2engine.com>
2017-04-25 19:25:01 +0000

    makeBuild.py: Version updated, and tag added: 6.6

--- x2engine/protected/config/X2Config.php
+++ x2engine/protected/config/X2Config.php
@@ -5,8 +5,8 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $dbname = 'x2engine';
-$version = '6.5.2';
-$buildDate = '1482441776';
+$version = '6.6';
+$buildDate = '1493148301';
 $updaterVersion = '6.0.3';
 $language='en';
 ?>
