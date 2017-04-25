83edaf8b561de14dee233c4d22df826fc3f76913
Josef Bustamante <kfbustam@ucsc.edu>
2017-04-25 00:58:34 -0700

    changed identity constant to user's id

--- x2engine/protected/components/sortableWidget/views/_liveChatProfileWidget.php
+++ x2engine/protected/components/sortableWidget/views/_liveChatProfileWidget.php
@@ -41,7 +41,7 @@
 <?php 
   $this->widget('YiiChatWidget',array(
         'chat_id'=>'123',                   // a chat identificator
-        'identity'=>1,                      // the user, Yii::app()->user->id ?
+        'identity'=>Yii::app()->user->id,                      // the user, Yii::app()->user->id ?
         'selector'=>'#chat',                // were it will be inserted
         'minPostLen'=>2,                    // min and
         'maxPostLen'=>10,                   // max string size for post
