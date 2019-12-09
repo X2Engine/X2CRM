365f1fa190ae949b11985b6547e9cdfbeec2d38e
System Administrator <root@Peters-MacBook-Pro.local>
2019-12-09 11:54:26 -0800

    8.0

--- x2engine/protected/config/X2Config.php
+++ x2engine/protected/config/X2Config.php
@@ -1,12 +1,12 @@
 <?php
-$appName='X2CRM';
-$email='admin@example.com';
-$host='localhost';
-$user='x2crm';
-$pass='test123';
-$dbname='x2crm';
-$version='8.0';
-$buildDate='1575664391';
-$updaterVersion='6.9.1';
+$appName = 'X2Engine';
+$email = 'a@b.com';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'x2engine';
+$version = '8.0';
+$buildDate = '1575664391';
+$updaterVersion = '6.9.1';
 $language='en';
 ?>
