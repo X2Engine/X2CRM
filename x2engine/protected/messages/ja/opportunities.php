328c5ce76c197949289d2435a6e710dd91d151b0
dg-hasimoto <hasimoto@designet.co.jp>
2021-09-09 15:56:42 +0900

    Fixed Japanese translation file

--- x2engine/protected/messages/ja/opportunities.php
+++ x2engine/protected/messages/ja/opportunities.php
@@ -1,10 +1,11 @@
 <?php return array (
-  'Opportunity Record Details' => 'セールスのレコードの詳細',
-  'Opportunities Stage' => 'チャンスステージ',
-  'Create {opportunity}' => '作成{opportunity}',
+  'Opportunity Record Details' => '案件情報の詳細',
+  'Opportunities Stage' => '案件の状況',
+  'Create {opportunity}' => '{opportunity}の作成',
   '{opportunity}:' => '{opportunity} ：',
   '{opportunities} List' => '{opportunities}一覧',
-  'Edit {opportunity}' => '編集{opportunity}',
-  'Import {opportunities}' => 'インポート{opportunities}',
-  'Export {opportunities}' => 'エクスポート{opportunities}',
+  'Edit {opportunity}' => '{opportunity}の編集',
+  'Import {opportunities}' => '{opportunities}のインポート',
+  'Export {opportunities}' => '{opportunities}のエクスポート',
+  'Create Opportunity' => '案件情報の作成',
 );
--- x2engine/protected/messages/ja/products.php
+++ x2engine/protected/messages/ja/products.php
@@ -1,9 +1,9 @@
 <?php return array (
-  'Inventory' => 'インベントリー',
+  'Inventory' => '在庫',
   'Product Information' => '製品情報',
   'Adjustments' => '調整',
   'Line Item' => 'ライン·アイテム',
   'Unit Price' => '単価',
-  'Create New {module}' => '新規作成{module}',
-  'Update {module}' => '更新{module}',
+  'Create New {module}' => '{module}の新規作成',
+  'Update {module}' => '{module}の更新',
 );
